e3e7807b5710babe7b197b2c69db934eea55727c
Andrii_Petrov2 <andrii_petrov2@epam.com>
2021-12-17 00:38:06 +0200

    add additional condition

--- newYearGame.js
+++ newYearGame.js
@@ -2,7 +2,7 @@ function mms(n, a) {
   if (n === 1) {
     return a[0] === 1 ? 'Ельф' : 'Олень';
   }
-
+  
   if (n % 2 === 0) {
     if (a.filter(el => el === 1).length  >= a.length - 1) {
       return 'Олень';
@@ -14,6 +14,11 @@ function mms(n, a) {
 
     return a.reduce((acc, el) => acc + el) % 2 === 0 ? 'Ельф' : 'Олень';
   } else {
+    if (a.filter(el => el === 1).length  === a.length - 1) {
+      return 'Олень';
+    }
+
+    
     return a.reduce((acc, el) => acc + el) % 2 === 1 ? 'Ельф' : 'Олень';
   }
 }
@@ -27,12 +32,12 @@ console.log(mms(2, [3, 2]));
 console.log(mms(2, [3, 3]));
 console.log(mms(2, [4, 3]));
 console.log(mms(3, [1, 1, 1]));
-console.log(mms(3, [1, 1, 3]));
-console.log(mms(3, [1, 1, 3]));
+console.log(mms(3, [1, 1, 5]));
+console.log(mms(3, [2, 2, 3]));
 console.log(mms(3, [1, 2, 3]));
 console.log(mms(3, [2, 2, 3]));
 console.log(mms(4, [1, 1, 1, 1]));
-console.log(mms(4, [1, 1, 1, 2]));
+console.log(mms(4, [1, 1, 1, 4]));
 console.log(mms(4, [1, 1, 2, 2]));
 console.log(mms(4, [1, 1, 2, 3]));
 console.log(mms(5, [1, 1, 1, 1, 1]));
