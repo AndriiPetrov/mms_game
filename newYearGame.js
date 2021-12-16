6de6ac22d39155e9df2291bdc3e69333be4d83bf
Andrii_Petrov2 <andrii_petrov2@epam.com>
2021-12-16 22:28:17 +0200

    add additional logic for 2

--- newYearGame.js
+++ newYearGame.js
@@ -8,6 +8,10 @@ function mms(n, a) {
       return 'Олень';
     }
 
+    if (n === 2) {
+      return a[0] === a[1] ? 'Ельф' : 'Олень';
+    }
+
     return a.reduce((acc, el) => acc + el) % 2 === 0 ? 'Ельф' : 'Олень';
   } else {
     return a.reduce((acc, el) => acc + el) % 2 === 1 ? 'Ельф' : 'Олень';
@@ -21,6 +25,7 @@ console.log(mms(2, [1, 2]));
 console.log(mms(2, [2, 2]));
 console.log(mms(2, [3, 2]));
 console.log(mms(2, [3, 3]));
+console.log(mms(2, [4, 3]));
 console.log(mms(3, [1, 1, 1]));
 console.log(mms(3, [1, 1, 3]));
 console.log(mms(3, [1, 1, 3]));
